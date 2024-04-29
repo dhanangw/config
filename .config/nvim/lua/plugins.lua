b7835bfddf7c5663a405cb52f1125768c9d36bd5
dhanangw <wibisono.dhanang4@gmail.com>
2024-04-29 14:15:37 +0900

    fix missing trailing-comma

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -59,7 +59,7 @@ return {
       init = function()
           vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
           vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-      end
+      end,
       config = function()
           vim.cmd.colorscheme("rose-pine")
       end
