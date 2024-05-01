10c01251ce8fb0835c4d6ebb285edda7559b16e1
dhanangw <wibisono.dhanang4@gmail.com>
2024-05-01 13:41:55 +0900

    format; add link to doc

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,13 +1,16 @@
-  return {
-	  'nvim-telescope/telescope.nvim',
-      version = '0.1.6',
-	  dependencies = {'nvim-lua/plenary.nvim'},
-      config = function()
-          local builtin = require('telescope.builtin')
+return {
+    'nvim-telescope/telescope.nvim',
+    version = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local builtin = require('telescope.builtin')
 
-          vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- Search for files with ripgrep.
-           vim.keymap.set('n', '<leader>pgf', builtin.git_files, {}) -- Search for files that's tracked by Git.
-          vim.keymap.set('n', '<leader>ps', function() builtin.live_grep() end)-- Search string occurences in current folder.
-      end
-  }
+        -- Other Telescope's capabilities, for consideration...
+        -- https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#pickers
 
+
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})             -- Search for files with ripgrep.
+        vim.keymap.set('n', '<leader>pgf', builtin.git_files, {})             -- Search for files that's tracked by Git.
+        vim.keymap.set('n', '<leader>ps', function() builtin.live_grep() end) -- Search string occurences in current folder.
+    end
+}
