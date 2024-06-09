e0c314dce174ae6d516baff1c09a31c6281d2e10
Dhanang Wibisono <wibisono.dhanang4@gmail.com>
2024-06-09 22:06:49 +0900

    add nvim-metals (#12)
    * move LSP on_attach function to remap.lua; add nvim-metals
    
    * update README
    
    * only activate nvim-metals for scala and sbt files
    
    * remote typo

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,3 +1,5 @@
+require("dhanangw.remap")
+
 return {
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -18,21 +20,7 @@ return {
         -- Keymaps
         vim.api.nvim_create_autocmd('LspAttach', {
             desc = 'LSP Actions',
-            callback = function(event)
-                local opts = { buffer = event.buf }
-
-                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-                vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end, opts)
-                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-            end
+            callback = LSP_ON_ATTACH
         })
 
         -- LSP server management
